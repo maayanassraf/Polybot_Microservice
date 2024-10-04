e5623bc1bfe8430983358b6b93447671e9d73202
maayanassraf <maayanassraf@gmail.com>
2024-10-04 16:31:41 +0300

    change code prod

--- bot.py
+++ bot.py
@@ -106,3 +106,4 @@ class ObjectDetectionBot(Bot):
             except:
                 logger.error('An error occurred while trying to send message to queue')
                 self.send_text((msg['chat']['id']), text=f'Something went wrong. Please try again...')
+                # note for check
